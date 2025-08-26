c516d2797d1ca89f9ec5a0e75d6460285f13dea4
znataniel <znataniel@protonmail.com>
2025-08-26 14:17:32 -0300

    ppm hello world

--- main.cpp
+++ main.cpp
@@ -1,20 +1,30 @@
 #include "vectors.h"
-using std::cout;
-using std::endl;
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+
+#define WIDTH 256
+#define HEIGHT 256
 
 int main(void) {
-  Vector papu(1, 2, 3);
-  Vector lince(2, 4, 6);
+  std::ofstream outfile("out.ppm", std::ios::binary);
 
-  cout << "Papu" << endl;
-  papu.display();
+  outfile << "P3" << " " << WIDTH << " " << HEIGHT << " " << "255" << "\n";
+  for (int j = 0; j < HEIGHT; j++) {
+    std::clog << "\rScanlines remaining: " << HEIGHT - j << " " << std::flush;
+    for (int i = 0; i < WIDTH; i++) {
+      double u = double(i) / (WIDTH - 1);
+      double v = double(j) / (HEIGHT - 1);
 
-  cout << "\nLince" << endl;
-  lince.display();
+      int ir = int(255.999 * u);
+      int ig = int(255.999 * v);
+      int ib = int(255.999 * 0.0);
 
-  Vector papulince = papu + lince;
-  cout << "\nPapuLince"<< endl;
-  papulince.display();
+      outfile << ir << " " << ig << " " << ib << '\n';
+    }
+  }
 
-  cout << "Locuron tronco" << endl;
+  outfile.close();
+  std::cout << "\rDone.                                      " << std::endl;
+  return EXIT_SUCCESS;
 }
